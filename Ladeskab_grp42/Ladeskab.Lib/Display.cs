9ec1745e62e6cd399815fdd1ce6df2a3841435bc
Christian-Bach-Johansen <1.christian.bach@gmail.com>
2021-03-11 13:19:24 +0100

    Display klasse udkast

---
+++ Ladeskab_grp42/Ladeskab.Lib/Display.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab.Lib
+{
+    class Display
+    {
+        public void DisplayMessage(string msg)
+        {
+            Console.WriteLine("New Message: " + msg);
+        }
+    }
+}
