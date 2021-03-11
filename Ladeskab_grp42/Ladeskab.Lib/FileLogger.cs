db1326c8a401cdfbbb448d32264e0fa2a9a2e0ba
Christian-Bach-Johansen <1.christian.bach@gmail.com>
2021-03-11 13:44:20 +0100

    Udkast til FileLogger skrevet

---
+++ Ladeskab_grp42/Ladeskab.Lib/FileLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ladeskab.Lib
+{
+    class FileLogger
+    {
+        public void LogFile()
+        {
+            string tempLog = "23";
+            string path = "C:/Users/1chri/Documents/GitHub/Ladeskab_Gruppe42/Ladeskab_grp42";
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("New log " + DateTime.Now + " : " + tempLog);
+            }
+        }
+    }
+}
