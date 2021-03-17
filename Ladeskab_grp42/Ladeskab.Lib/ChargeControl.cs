064c3ecd25975a045b5d61178a107ed0d76e3fe0
SimonPhiDang <201705957@post.au.dk>
2021-03-17 09:32:27 +0100

    Opdateret ChargeControl med håndtering af OnCurrentEvent
    StartCharge,StopCharge,IsConnected, EvalChargeState Implementeret. Display og Usb charger refereret til

--- Ladeskab_grp42/Ladeskab.Lib/ChargeControl.cs
+++ Ladeskab_grp42/Ladeskab.Lib/ChargeControl.cs
@@ -3,32 +3,132 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UsbSimulator;
 
 namespace Ladeskab.Lib
 {
-	class ChargeControl
+	public interface IChargeControl
 	{
-		private IUsbCharger _usbCharger;
-		public event EventHandler<CurrentEventArgs> CurrentValueEvent;
-		bool IsConnected()
+		void StartCharge();
+		void StopCharge();
+		bool IsConnected();
+	}
+
+	class ChargeControl : IChargeControl
+	{
+		//Forbindelser
+		private readonly IUsbCharger _usbCharger;
+		private readonly IDisplay _display;
+		//States
+		private enum ChargerState
 		{
-			if(_usbCharger.Connected)
-				return false;
-			else
-			{ 
-				return true;
+			Idle,
+			IsCharging,
+			OverCurrentFail,
+			FullyCharged
+		};
+
+		//Ladesituationer 
+		private ChargerState _chargerState = ChargerState.Idle;
+		private ChargerState _latestState = ChargerState.Idle;
+		public int ReadChargerState = -1;
+
+		//Max_min værdier for charging current
+		private const double MaximumChargeCurrent = 500.000;
+		private const double MinimumChargeCurrent = 5.000;
+		private const double ZeroChargeCurrent = 0.000;
+		//attributter
+		private double _chargeCurrent = 0.0;
+		public double ReadChargeCurrent = -1.0;
+
+		public ChargeControl(IDisplay display, IUsbCharger usbCharger)
+		{
+			//Use Display
+			_display = display;
+			// Use USBCharger
+			_usbCharger = usbCharger;
+			//Attach to events fra USB charger
+			this._usbCharger.CurrentValueEvent += OnChargeCurrUpdate;
+
+		}
+
+		public bool IsConnected()
+		{
+			return _usbCharger.Connected;
+		}
+
+		public void StartCharge()
+		{
+			_chargerState = ChargerState.Idle;
+			_usbCharger.StartCharge();
+			// Display start charging message
+		}
+
+		public void StopCharge()
+		{
+			_chargerState = ChargerState.Idle;
+			_usbCharger.StopCharge();
+			//Display stop charging message
+		}
+
+		private void OnChargeCurrUpdate(object sender, CurrentEventArgs a)
+		{
+			_chargeCurrent = a.Current;
+			ReadChargeCurrent = _chargeCurrent;
+
+			EvalChargeState();
+			UpdateDisplay();
+		}
+
+		//EvalChargeState
+		private void EvalChargeState()
+		{
+			if (_chargeCurrent > MaximumChargeCurrent) //If chargecurrent over maxcharge 
+			{
+				_usbCharger.StartCharge();
+				_chargerState = ChargerState.OverCurrentFail;
 			}
-		}
+			else if (_chargeCurrent <= MaximumChargeCurrent && _chargeCurrent > MinimumChargeCurrent) // Indenfor 5mA og 500 mA
+			{
+				_chargerState = ChargerState.IsCharging;
+			}
+			else if (_chargeCurrent > ZeroChargeCurrent && _chargeCurrent <= MinimumChargeCurrent) // Fully charged
+			{
+				_chargerState = ChargerState.FullyCharged;
+			}
+			else
+			{
+				if (_chargerState == ChargerState.OverCurrentFail) return;
 
-		void StartCharge()
-		{
-			CurrentValueEvent.Invoke(this,new CurrentEventArgs());
-		}
+				_chargerState = ChargerState.Idle;
+			}
 
-		void StopCharge()
+			ReadChargerState = (int)_chargerState;
+
+		}
+		private void UpdateDisplay()
 		{
-			CurrentValueEvent.Invoke(this, new CurrentEventArgs());
+
+			switch (_chargerState)
+			{
+				case ChargerState.OverCurrentFail:   //State charge fail
+					_display.DisplayMessage("Current failed");
+					_latestState = ChargerState.OverCurrentFail;
+					break;
+				case ChargerState.FullyCharged:
+					_display.DisplayMessage("Fully Charged");
+					_latestState = ChargerState.FullyCharged;
+					break;
+				case ChargerState.IsCharging:
+					_display.DisplayMessage("Is Charging");
+					_latestState = ChargerState.IsCharging;
+					break;
+				case ChargerState.Idle:
+					_display.DisplayMessage("Idle");
+					_latestState = ChargerState.Idle;
+					break;
+				default:
+					break;
+			}
 		}
 
 	}
