83397feca1f56f375fd37449e427313bd0093c4e
Christian-Bach-Johansen <1.christian.bach@gmail.com>
2021-03-17 09:27:16 +0100

    Added OnDoorClose and OnDoorOpen to Door.cs

--- Ladeskab_grp42/Ladeskab.Lib/StationControl.cs
+++ Ladeskab_grp42/Ladeskab.Lib/StationControl.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UsbSimulator;
 
-namespace Ladeskab
+namespace Ladeskab.Lib
 {
     public class StationControl
     {
@@ -21,6 +21,7 @@ namespace Ladeskab
         // Her mangler flere member variable
         private LadeskabState _state;
         private IUsbCharger _charger;
+        private IDoor _door;
         private int _oldId;
 
         private string logFile = "logfile.txt"; // Navnet på systemets log-fil
