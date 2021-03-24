fcd45acbbf29246bb7604fd6f7c401d1df8ed22d
Christian-Bach-Johansen <1.christian.bach@gmail.com>
2021-03-24 08:38:04 +0100

    Oprettet Test til FileLogger

---
+++ Ladeskab_grp42/Ladeskab.Unit.Test/FileLoggerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Ladeskab.Unit.Test
+{
+    [TestClass]
+    class FileLoggerTest
+    {
+        private FileLogger _uut;
+
+
+    }
+}
