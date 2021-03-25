42b23dd84ed06c1f3ca4c0b83ba66c22c8f10ea1
Christian-Bach-Johansen <1.christian.bach@gmail.com>
2021-03-25 09:14:10 +0100

    Startet på filelogger test

---
+++ Ladeskab_grp42/Ladeskab.Lib/FileWriter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Ladeskab.Lib
+{
+    public class FileWriter
+    {
+        public void WriteFile(string path, string logmsg)
+        {
+            using (StreamWriter sw = File.AppendText(path))
+            {
+
+
+                sw.WriteLine("New log " + DateTime.Now + " : " + logmsg);
+            }
+        }
+    }
+}
---
+++ Ladeskab_grp42/Ladeskab.Unit.Test/FileLoggerUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using NSubstitute;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Ladeskab.Unit.Test
+{
+    [TestFixture]
+    class FileLoggerUnitTest
+    {
+        private FileLogger _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void FileLoggerUnitTest_NoFileWrite_NewFileCreated()
+        {
+            var fakeFileWriter = Substitute.For<FileWriter>();
+            var fakeFileReader = Substitute.For<FileReader>();
+
+            _uut = new FileLogger(fakeFileWriter, fakeFileReader);
+
+
+
+            Console.getCurrentdirectory
+
+            //Open() for at se om findes
+            //
+            //Bliver fil oprettet
+            //read funktion så kan om er skrevet rigtigt
+            //Assert does exist
+        }
+
+
+
+    }
+}
